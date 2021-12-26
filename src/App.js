ab3e886fa9ba4f2ec5c438cb07ac5a98b70f1fbf
Imam Hilmi <imam.hilmi253@gmail.com>
2021-12-26 18:10:12 +0700

    Add some pages and use react-router-dom to website

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import './App.css';
 import Header from '../src/components/Header/Header'
+import { BrowserRouter, Routes, Route, } from "react-router-dom"
+import SimpleBottomNavigation from '../src/components/MainNav'
+import { Container } from '@mui/material';
+import Trending from '../src/components/Pages/Trending/Trending'
+import Movies from '../src/components/Pages/Movies/Movies'
+import Series from '../src/components/Pages/Series/Series'
+import Search from '../src/components/Pages/Search/Search'
 
 function App() {
   return (
     <>
       <Header />
       <div className="App">
-        <h3>isi content</h3>
+        <Container>
+          {/* <BrowserRouter> */}
+            <Routes>
+              <Route exact path="/" element={<Trending />} />
+              <Route exact path="/movies" element={<Movies />} />
+              <Route exact path="/series" element={<Series />} />
+              <Route exact path="/search" element={<Search />} />
+            </Routes>
+          {/* </BrowserRouter> */}
+        </Container>
       </div>
+      <SimpleBottomNavigation />
     </>
   );
 }
---
+++ src/components/MainNav.js
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Box from '@mui/material/Box';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import MovieIcon from '@mui/icons-material/Movie';
+import ComputerIcon from '@mui/icons-material/Computer';
+import TrendingUpIcon from '@mui/icons-material/TrendingUp';
+import SearchIcon from '@mui/icons-material/Search';
+
+export default function SimpleBottomNavigation() {
+    const [value, setValue] = React.useState(0);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (value === 0)
+            navigate("/");
+        else if (value === 1)
+            navigate("/movies");
+        else if (value === 2)
+            navigate("/series");
+        else if (value === 3)
+            navigate("/search");
+    }, [value, navigate])
+
+  return (
+    <Box sx={{ 
+        width: "100%",
+        position: "fixed",
+        bottom: 0,
+        boxShadow: 3,
+        // bgcolor: 'background.paper',
+        zIndex: 100,
+     }}>
+      <BottomNavigation
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      >
+        <BottomNavigationAction style={{color:"#222942"}}  label="Trending" icon={<TrendingUpIcon />} />
+        <BottomNavigationAction style={{color:"#222942"}}  label="Movies" icon={<MovieIcon />} />
+        <BottomNavigationAction style={{color:"#222942"}}  label="TV Series" icon={<ComputerIcon />} />
+        <BottomNavigationAction style={{color:"#222942"}}  label="Search" icon={<SearchIcon />} />
+      </BottomNavigation>
+    </Box>
+  );
+}
